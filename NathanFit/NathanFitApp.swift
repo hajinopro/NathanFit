7a305e58bb2fbde8021aa017cbc206000b2f35b1
Nathan <hajinopro@gmail.com>
2021-08-26 16:44:06 +0900

    Initial Commit

---
+++ NathanFit/NathanFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  NathanFitApp.swift
+//  NathanFit
+//
+//  Created by Nathan on 2021/08/26.
+//
+
+import SwiftUI
+
+@main
+struct NathanFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
